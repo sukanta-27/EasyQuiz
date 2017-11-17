ad753628b251c4af1f95de3ba3a11072b54b2e31
Sukanta Roy <sukanta.raj.27@gmail.com>
2017-11-17 22:19:24 +0530

    Add Reset feature in Quiz Page

--- app/src/main/java/com/example/android/easyquiz/QuizActivity.java
+++ app/src/main/java/com/example/android/easyquiz/QuizActivity.java
@@ -3,6 +3,10 @@ package com.example.android.easyquiz;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.RadioGroup;
+
+import static com.example.android.easyquiz.R.id.radioGroup;
 
 /**
  * Created by sukanta on 11/11/2017.
@@ -15,6 +19,25 @@ import android.support.v7.app.AppCompatActivity;
             setContentView(R.layout.activity_quiz);
             Intent intent = getIntent();
 
+        }
+
+    /**
+     * This method will reset all the checked RadioButtons in all four of the questions.
+     * @param view
+     */
+
+    public void resetQuiz (View view){
+            RadioGroup radioGroup_one = (RadioGroup) findViewById(R.id.group_one);
+            radioGroup_one.clearCheck();
+
+            RadioGroup radioGroup_two = (RadioGroup) findViewById(R.id.group_two);
+            radioGroup_two.clearCheck();
+
+            RadioGroup radioGroup_three = (RadioGroup) findViewById(R.id.group_three);
+            radioGroup_three.clearCheck();
+
+            RadioGroup radioGroup_four = (RadioGroup) findViewById(R.id.group_four);
+            radioGroup_four.clearCheck();
         }
     }
 
